d9329f7a385e201646a24101610f2c2c9481cb4e
Reza Mahendra Dhata <rezamahendradhata@gmail.com>
2021-02-21 05:13:40 +0700

    detik scrapy Baru

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ populer_area = soup.find(attrs={'class':'grid-row list-content'})
 titles = soup.find_all(attrs={'class':'media__title'})
 images = soup.find_all(attrs={'class':'media__image'})
 
+
+
 for image in images:
     print(image.find('a').find('img')['title'])
 
--- run.py
+++ run.py
@@ -3,13 +3,24 @@ from bs4 import BeautifulSoup
 
 from flask import Flask, render_template
 
+from main import image
+
 app= Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'test'
+    return render_template('index.html')
+@app.route('/detik-populer')
+def detik_populer():
+    html_doc = requests.get('https://www.detik.com/terpopuler', params={'tag_from': 'wp_cb_mostPopular_more'})
 
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
 
+    populer_area = soup.find(attrs={'class': 'grid-row list-content'})
+    titles = soup.find_all(attrs={'class': 'media__title'})
+    images = soup.find_all(attrs={'class': 'media__image'})
 
-if __name__=='_main_':
+    return render_template('index.html',image=image)
+
+if __name__=='__main__':
     app.run(debug=True)
